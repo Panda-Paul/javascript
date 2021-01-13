f807a11bdb0c657225b8c4ddb8fd4702fe690a85
PandaPaul <76727882+Panda-Paul@users.noreply.github.com>
2021-01-13 15:56:07 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,48 @@
+//---------------------X_variablen
+let Xpos = 200;
+let Xspeed = 3;
+//--------------------Y_variablen
+let Ypos = 200;
+let Yspeed = 3;
 
+let size = 50;
+
+function setup() {
+  createCanvas(400, 400);
+  rectMode(CENTER);
+      fill(255, 0, 251);
+  Xpos = random(0, width - size)
+  Ypos = random(0, height - size)
+}
+
+function draw() {
+  background(0, 221, 255);
+  console.log("Xpos: " + Xpos + "   Ypos:  " + Ypos)
+  
+  Xpos = Xpos + Xspeed;
+  Ypos = Ypos + Yspeed;
+  
+  if(Xpos >= width - size/2){
+    Xspeed = Xspeed * -1
+    fill(random(0, 255), random(0, 255), random(0, 255))
+  }
+  if(Xpos <= size/2){
+    Xspeed = Xspeed * -1
+    fill(random(0, 255), random(0, 255), random(0, 255))
+  }
+  if(Ypos >= height - size/2){
+    Yspeed = Yspeed * -1
+    fill(random(0, 255), random(0, 255), random(0, 255))
+  }
+  if(Ypos <= size/2){
+    Yspeed = Yspeed * -1
+    fill(random(0, 255), random(0, 255), random(0, 255))
+  }
+
+  
+//   console.log("hi")
+  
+  
+    ellipse(Xpos, Ypos, size);
+//  line(0, 0, 400, 400)
+}
